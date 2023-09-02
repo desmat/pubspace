d31e1a46b414516d318fbb626c12cdf1c88075b0
Mathieu Desjarlais <github@desmat.ca>
2023-09-02 16:11:22 -0400

    troubleshoot build error for user.ts

--- app/_components/ProfileNavLink.tsx
+++ app/_components/ProfileNavLink.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 import { FaRegUserCircle, FaUserCircle } from 'react-icons/fa';
-import useUser from '@/app/_hooks/user';
+import useUser from '@/app/_hooks/User';
 
 export default function ProfileNavLink({
   href, className,
