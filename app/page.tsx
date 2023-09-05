2c39e76013d48b31fd6170200ef116b69aa5c6d6
Mathieu Desjarlais <github@desmat.ca>
2023-09-05 17:48:36 -0400

    Update readme and landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ export default function Page() {
 
   return (
     <main className="flex flex-col items-center _justify-between _p-24">
-      <h1>Welcome to PubSpace!</h1>
-      <p>
-        Things for pubs, coffee shops, public spaces, etc.
-      </p>
+      <h1>PubSpace: The app for public spaces!</h1>
+      <p>An app for public spaces (pubs, cafés, etc) to manage and publish bulletin boards, menus, event calendars, maybe even trivia and other pub games.</p>
+      <p>Just a basic bulletin board for now, more to come soon!</p>
       <p>
         <a href="/profile">Signup</a> today!
       </p>
