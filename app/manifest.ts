ab50ec9f507346e64b20b1bbabee9174b0bed93f
Mathieu Desjarlais <github@desmat.ca>
2023-10-25 13:25:55 -0400

    manifest file

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'PubSpace: The app for public spaces',
+    short_name: 'PubSpace',
+    description: 'PubSpace: The app for public spaces',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#264653',
+    theme_color: '#2a9d8f',
+    icons: [
+      {
+        src: 'favicon.ico',
+        sizes: '48x48',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
