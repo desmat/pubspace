1df66483fe88b52354da5ccd8161ed9c4ee408ea
Mathieu Desjarlais <github@desmat.ca>
2023-11-23 17:00:27 -0500

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="flex flex-col items-left lg:items-center lg:max-w-4xl lg:mx-auto px-4">
       <h1 className="text-center">PubSpace: The app for public spaces!</h1>
-      <p>A "toy" web app exploring AI-powered utilities for public spaces (pubs, cafés, etc): manage and publish bulletin boards, menus, event calendars, trivia and other pub games.</p>
+      <p>A &ldquo;toy&ldquo; web app exploring AI-powered utilities for public spaces (pubs, cafés, etc): manage and publish bulletin boards, menus, event calendars, trivia and other pub games.</p>
       <p>Single tenant, a basic bulletin board and trivia game generator for now, more to come soon!</p>
     </main>
   )
